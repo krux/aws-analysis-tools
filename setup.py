9595233abdcc9dda4be29cbfd7c2d77a138778b5
Jos Boumans <jos@dwim.org>
2014-06-30 16:17:54 -0700

    * need to put all the deps in setup.py, or we get version mismatches :(

--- setup.py
+++ setup.py
@@ -10,17 +10,32 @@ Package setup for aws-analysis-tools
 ######################
 from __future__ import absolute_import
 from setuptools import setup, find_packages
+from pip.req    import parse_requirements
 
 import os
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION      = '0.0.2'
+VERSION      = '0.0.3'
 
 # URL to the repository on Github.
 REPO_URL     = 'https://github.com/krux/aws-analysis-tools'
 # Github will generate a tarball as long as you tag your releases, so don't
 # forget to tag!
 DOWNLOAD_URL = ''.join((REPO_URL, '/tarball/release/', VERSION))
+
+# We want to install all the dependencies of the library as well, but we
+# don't want to duplicate the dependencies both here and in
+# requirements.pip. Instead we parse requirements.pip to pull in our
+# dependencies.
+BASE_DIR     = os.path.dirname(os.path.abspath(__file__))
+REQUIREMENTS = os.path.join(BASE_DIR, 'requirements.pip')
+
+# A requirement file can contain comments (#) and can include some other
+# files (--requirement or -r), so we need to use pip's parser to get the
+# final list of dependencies.
+ALL_DEPENDENCIES = set([unicode(package.req)
+                    for package in parse_requirements(REQUIREMENTS)])
+
 
 setup(
     name             = 'aws-analysis-tools',
@@ -32,12 +47,7 @@ setup(
     download_url     = DOWNLOAD_URL,
     license          = 'All Rights Reserved.',
     packages         = find_packages(),
-    install_requires = [
-        'krux-stdlib',
-        'krux-boto',
-        'pyyaml',
-        'texttable',
-    ],
+    install_requires = ALL_DEPENDENCIES,
     entry_points     = {
         'console_scripts': [
             'krux-search-ec2-tags = search_ec2_tags:main',
