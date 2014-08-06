89c3c82d8fb28dedab5dcb971e83d9f77bee1a60
Jos Boumans <jos@dwim.org>
2014-08-06 16:53:39 -0700

    * up the version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ from pip.req    import parse_requirements
 import os
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION      = '0.0.3'
+VERSION      = '0.0.4'
 
 # URL to the repository on Github.
 REPO_URL     = 'https://github.com/krux/aws-analysis-tools'
