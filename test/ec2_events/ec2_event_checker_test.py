da3c7248af3f846b157f02a1ca63041b62dada8d
Peter Han <phan@krux.com>
2016-03-15 16:35:37 -0700

    Skeleton unit test suite

---
+++ test/ec2_events/ec2_event_checker_test.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+#
+# © 2015 Krux Digital, Inc.
+#
+
+#
+# Standard libraries
+#
+
+from __future__ import absolute_import
+import unittest
+from logging import Logger
+
+#
+# Third party libraries
+#
+
+from mock import MagicMock, patch
+
+#
+# Internal libraries
+#
+
+from aws_analysis_tools.ec2_events.ec2_event_checker import EC2EventChecker
+
+
+class EC2EventCheckerTest(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_init(self):
+        pass
+
+    def test_add_listener(self):
+        pass
+
+    def test_notify_event(self):
+        pass
+
+    def test_notify_complete(self):
+        pass
+
+    def test_check(self):
+        pass
