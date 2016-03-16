89eba2b6630b36461ac83745c351e434c301549c
Peter Han <phan@krux.com>
2016-03-15 20:06:39 -0700

    Basic test for check()

--- test/ec2_events/ec2_event_checker_test.py
+++ test/ec2_events/ec2_event_checker_test.py
@@ -25,12 +25,49 @@ from aws_analysis_tools.ec2_events.ec2_event_checker import EC2EventChecker
 
 
 class EC2EventCheckerTest(unittest.TestCase):
+    REGIONS = ['test-region-1', 'cn-test-region-2', 'test-gov-region-3']
+    DESCRIPTIONS = ['scheduled reboot', '[Completed]', '[Canceled]']
+    INSTANCE_NAME = 'unit-test.krxd.net'
 
     def setUp(self):
-        pass
+        regions = []
+        for region in self.REGIONS:
+            mock = MagicMock()
+            mock.name = region
+            regions.append(mock)
 
-    def test_init(self):
-        pass
+        ec2 = MagicMock(
+            regions=MagicMock(
+                return_value=regions,
+            ),
+        )
+
+        connection = MagicMock(
+            get_all_instance_status=MagicMock(
+                return_value=[MagicMock(events=[
+                    MagicMock(description=d) for d in self.DESCRIPTIONS
+                ])]
+            ),
+            get_only_instances=MagicMock(
+                return_value=[MagicMock(tags={'Name': self.INSTANCE_NAME})]
+            )
+        )
+        self._boto = MagicMock(
+            ec2=ec2,
+            connect_ec2=MagicMock(
+                return_value=connection
+            ),
+        )
+
+        self._logger = MagicMock(
+            spec=Logger,
+            autospec=True,
+        )
+
+        self._checker = EC2EventChecker(
+            boto=self._boto,
+            logger=self._logger,
+        )
 
     def test_add_listener(self):
         pass
@@ -42,4 +79,10 @@ class EC2EventCheckerTest(unittest.TestCase):
         pass
 
     def test_check(self):
-        pass
+        self._checker.check()
+
+        debug_calls = [
+            (('Checking region: %s', self.REGIONS[0]),),
+            (('Found following event: %s => %s', self.INSTANCE_NAME, self.DESCRIPTIONS[0]),),
+        ]
+        self.assertEqual(debug_calls, self._logger.debug.call_args_list)
