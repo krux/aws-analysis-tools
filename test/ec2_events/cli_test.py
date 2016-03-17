5535d1aa67b0d5df58cda07cefabe57482fddda4
Peter Han <phan@krux.com>
2016-03-17 16:50:47 -0700

    Skeleton CLI test

---
+++ test/ec2_events/cli_test.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# © 2015 Krux Digital, Inc.
+#
+
+#
+# Standard libraries
+#
+
+from __future__ import absolute_import
+import unittest
+
+#
+# Third party libraries
+#
+
+from mock import MagicMock, patch
+
+#
+# Internal libraries
+#
+
+from aws_analysis_tools.ec2_events.cli import Application
+
+
+class CLItest(unittest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_add_cli_arguments(self):
+        pass
+
+    def test_run(self):
+        pass
+
+    def test_main(self):
+        pass
