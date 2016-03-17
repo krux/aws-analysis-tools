bc261631ad98cfb5af67ccce6d74d2ef32d64cc7
Peter Han <phan@krux.com>
2016-03-16 18:33:53 -0700

    Skeleton Jira listener

---
+++ test/ec2_events/jira_listener_test.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+#
+# © 2015 Krux Digital, Inc.
+#
+
+#
+# Standard libraries
+#
+
+from __future__ import absolute_import
+import unittest
+
+#
+# Third party libraries
+#
+
+from mock import MagicMock, patch
+
+#
+# Internal libraries
+#
+
+from aws_analysis_tools.ec2_events.jira_listener import JiraListener
+
+
+class JiraListenerTest(unittest.TestCase):
+    APP_USERNAME = 'username'
+    APP_PASSWORD = 'password'
+    APP_BASE_URL = 'https://unit-test.jira.com/'
+
+    def setUp(self):
+        self._res = MagicMock(
+            json=MagicMock(
+                return_value=[{'issues': [{'key': 'FAKE-1234'}]}]
+            )
+        )
+        self._request = MagicMock()
+
+        with patch('aws_analysis_tools.ec2_events.jira_listener.request', self._request):
+            self._listener = JiraListener(
+                username=self.APP_USERNAME,
+                password=self.APP_PASSWORD,
+                base_url=self.APP_BASE_URL,
+            )
+
+    def test_handle_event(self):
+        pass
+
+    def test_handle_complete(self):
+        pass
