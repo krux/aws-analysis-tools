9c71603f4d4e16263572ce3d2b32979ec8f3e2f4
Peter Han <phan@krux.com>
2016-03-17 17:23:32 -0700

    Remove setup and add_cli_arguments() test

--- test/ec2_events/cli_test.py
+++ test/ec2_events/cli_test.py
@@ -30,17 +30,6 @@ class CLItest(unittest.TestCase):
     JIRA_BASE_URL = 'https://unit-test.example.com/'
     FLOW_TOKEN = 'fake-flow-token'
 
-    @patch.object(sys, 'argv', ['prog', '--jira-username', JIRA_USERNAME, '--jira-password', JIRA_PASSWORD, '--jira-base-url', JIRA_BASE_URL, '--flowdock-token', FLOW_TOKEN])
-    @patch('aws_analysis_tools.ec2_events.cli.EC2EventChecker')
-    @patch('aws_analysis_tools.ec2_events.cli.FlowdockListener')
-    @patch('aws_analysis_tools.ec2_events.cli.JiraListener')
-    def setUp(self, mock_jira, mock_flowdock, mock_checker):
-        self._jira = mock_jira
-        self._flowdock = mock_flowdock
-        self._checker = mock_checker
-
-        self._app = Application()
-
     @patch('aws_analysis_tools.ec2_events.cli.EC2EventChecker')
     @patch('aws_analysis_tools.ec2_events.cli.FlowdockListener')
     @patch('aws_analysis_tools.ec2_events.cli.JiraListener')
@@ -59,33 +48,44 @@ class CLItest(unittest.TestCase):
         self.assertFalse(mock_flowdock.called)
         self.assertFalse(mock_jira.called)
 
-    def test_init_all_set(self):
+    @patch.object(sys, 'argv', ['prog', '--jira-username', JIRA_USERNAME, '--jira-password', JIRA_PASSWORD, '--jira-base-url', JIRA_BASE_URL, '--flowdock-token', FLOW_TOKEN])
+    @patch('aws_analysis_tools.ec2_events.cli.EC2EventChecker')
+    @patch('aws_analysis_tools.ec2_events.cli.FlowdockListener')
+    @patch('aws_analysis_tools.ec2_events.cli.JiraListener')
+    def test_init_all_set(self, mock_jira, mock_flowdock, mock_checker):
         """
         Flowdock and JIRA listeners are created correctly when all CLI arguments are passed
         """
-        self._checker.assert_called_once_with(
-            boto=self._app.boto,
+        app = Application()
+
+        mock_checker.assert_called_once_with(
+            boto=app.boto,
             name=NAME,
-            logger=self._app.logger,
-            stats=self._app.stats
+            logger=app.logger,
+            stats=app.stats
         )
-        self._flowdock.assert_called_once_with(
+        mock_flowdock.assert_called_once_with(
             flow_token=self.FLOW_TOKEN,
             name=NAME,
-            logger=self._app.logger,
-            stats=self._app.stats
+            logger=app.logger,
+            stats=app.stats
         )
-        self._jira.assert_called_once_with(
+        mock_jira.assert_called_once_with(
             username=self.JIRA_USERNAME,
             password=self.JIRA_PASSWORD,
             base_url=self.JIRA_BASE_URL,
             name=NAME,
-            logger=self._app.logger,
-            stats=self._app.stats
+            logger=app.logger,
+            stats=app.stats
         )
 
     def test_add_cli_arguments(self):
-        pass
+        app = Application()
+
+        self.assertIn('flowdock_token', app.args)
+        self.assertIn('jira_username', app.args)
+        self.assertIn('jira_password', app.args)
+        self.assertIn('jira_base_url', app.args)
 
     def test_run(self):
         pass
