db560cd4f18fb6013c69b76b323cc9a0f3bb25c9
Peter Han <phan@krux.com>
2016-10-21 01:10:17 -0700

    Linting

--- aws_analysis_tools/cli/update_ec2_tags.py
+++ aws_analysis_tools/cli/update_ec2_tags.py
@@ -25,15 +25,15 @@ EC2_CLASS_KEY = 's_classes'
 class Application(krux_boto.Application):
 
     def __init__(self):
-        ### Call superclass to get krux-stdlib
-        super(Application, self).__init__(name = 'update-ec2-tags')
+        # Call superclass to get krux-stdlib
+        super(Application, self).__init__(name='update-ec2-tags')
 
-        self.instance_id    = self.args.instance_id
-        self.ec2_region     = self.args.ec2_region
-        self.environment    = self.args.environment
-        self.cluster_name   = self.args.cluster_name
-        self.classes        = self.args.classes
-        self.dry_run        = self.args.dry_run
+        self.instance_id = self.args.instance_id
+        self.ec2_region = self.args.ec2_region
+        self.environment = self.args.environment
+        self.cluster_name = self.args.cluster_name
+        self.classes = self.args.classes
+        self.dry_run = self.args.dry_run
 
     def add_cli_arguments(self, parser):
 
@@ -43,116 +43,114 @@ class Application(krux_boto.Application):
 
         group.add_argument(
             '--instance-id',
-            help = "The instance id of the instance. Example: 'i-27459bcc' "
-            "(output of `facter ec2_instance_id`)"
+            help=("The instance id of the instance. Example: 'i-27459bcc' "
+                  "(output of `facter ec2_instance_id`)")
         )
 
         group.add_argument(
             '--ec2-region',
-            required = True,
-            help     = "EC2 region to use. Example: 'us-east-1'. "
-            "NB: This is the *region*, not the *availability zone*."
+            required=True,
+            help=("EC2 region to use. Example: 'us-east-1'. "
+                  "NB: This is the *region*, not the *availability zone*.")
         )
 
         group.add_argument(
             '--environment',
-            default = 'dev',
-            help    = "The environment to set. (default: %(default)s)."
+            default='dev',
+            help="The environment to set. (default: %(default)s)."
         )
 
         group.add_argument(
             '--cluster-name',
-            required = True,
-            help     = "The cluster name to set. Example: 'apiservices-a'"
+            required=True,
+            help="The cluster name to set. Example: 'apiservices-a'"
         )
 
         group.add_argument(
             '--classes',
-            nargs   = "*",
-            default = [],
-            help    = "Specify class to tag with (can be used multiple times). "
-            "(default: %(default)s)"
+            nargs="*",
+            default=[],
+            help=("Specify class to tag with (can be used multiple times). "
+                  "(default: %(default)s)")
         )
 
         group.add_argument(
             '--dry-run',
-            default = False,
-            action  = 'store_true',
-            help    = "Do a dry run; find the tags, but do not update them "
-            "in EC2. (default: %(default)s)"
+            default=False,
+            action='store_true',
+            help=("Do a dry run; find the tags, but do not update them "
+                  "in EC2. (default: %(default)s)")
         )
 
-    def update_tags(self,
-        ec2_region      = None,
-        instance_id     = None,
-        classes         = None,
-        environment     = None,
-        cluster_name    = None,
-        dry_run         = False,
+    def update_tags(
+        self,
+        ec2_region=None,
+        instance_id=None,
+        classes=None,
+        environment=None,
+        cluster_name=None,
+        dry_run=False,
     ):
-        log   = self.logger
+        log = self.logger
         stats = self.stats
 
-        ### from cli or passed explicitly?
-        ec2_region      = ec2_region    or self.ec2_region
-        instance_id     = instance_id   or self.instance_id
-        classes         = classes       or self.classes
-        dry_run         = dry_run       or self.dry_run
-        environment     = environment   or self.environment
-        cluster_name    = cluster_name  or self.cluster_name
-
+        # from cli or passed explicitly?
+        ec2_region = ec2_region or self.ec2_region
+        instance_id = instance_id or self.instance_id
+        classes = classes or self.classes
+        dry_run = dry_run or self.dry_run
+        environment = environment or self.environment
+        cluster_name = cluster_name or self.cluster_name
 
         with stats.timing('update_tags'):
 
-            ### without region or instance id, we can't proceed
+            # without region or instance id, we can't proceed
             if not ec2_region or not instance_id:
                 self.raise_critical_error(
-                    'Could not determine instance_id (%s) and/or ec2_region (%s)' % \
+                    'Could not determine instance_id (%s) and/or ec2_region (%s)' %
                     (instance_id, ec2_region)
                 )
 
-            ### many places the info can come from, so let us know here what we found
+            # many places the info can come from, so let us know here what we found
             log.info(
                 'Proceeding with instance id: %s - region: %s - classes: %s',
                 instance_id, ec2_region, ' '.join(classes)
             )
 
-            ###
-            ### Set up the tags we want to use
-            ###
+            # Set up the tags we want to use
             tags_dict = {
-                'environment':  environment,
+                'environment': environment,
                 'cluster_name': cluster_name,
-                EC2_CLASS_KEY:  ",".join(classes),
+                EC2_CLASS_KEY: ",".join(classes),
             }
 
-            ### quick dump of what we're about to do send.
-            for k,v in tags_dict.iteritems():
+            # quick dump of what we're about to do send.
+            for k, v in tags_dict.iteritems():
                 log.info('Setting tag "%s" to: %s', k, v)
 
-
-            ###
-            ### Now do the actual update, if desired
-            ###
+            #
+            # Now do the actual update, if desired
+            #
             if dry_run:
                 log.info('Dry run - not updating instance in EC2')
 
             else:
                 log.info('Updating instance %s in region %s', instance_id, ec2_region)
-                region_obj  = self.boto.ec2.get_region(ec2_region)
-                ec2         = self.boto.connect_ec2(region = region_obj)
+                region_obj = self.boto.ec2.get_region(ec2_region)
+                ec2 = self.boto.connect_ec2(region=region_obj)
 
-                ### ec2 calls throw exceptions when they fail
+                # ec2 calls throw exceptions when they fail
                 try:
                     ec2.create_tags([instance_id], tags_dict)
                     stats.incr('ec2_tag_update')
                     log.info('Update completed successfully')
 
-                ### no matter what happens, we want to catch it and make sure
-                ### log it appropriately before we re-raise
+                # no matter what happens, we want to catch it and make sure
+                # log it appropriately before we re-raise
                 except Exception, e:
                     stats.incr('error.ec2_tag_update')
                     self.raise_critical_error('Tag update failed: %s' % e)
+
 
 def main():
     app = Application()
