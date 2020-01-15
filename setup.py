e2f646fd547ede96e7a4bdb8e5d55263f1cf5655
Rich Braun <rich.braun@salesforce.com>
2020-01-15 11:35:28 -0800

    W-7089157 deal with NoneType exception for terminated instances

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from __future__ import absolute_import
 from setuptools import setup, find_packages
 
 # We use the version to construct the DOWNLOAD_URL.
-VERSION      = '0.6.5'
+VERSION      = '0.6.6'
 
 # URL to the repository on Github.
 REPO_URL     = 'https://github.com/krux/aws-analysis-tools'
